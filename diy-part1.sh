c066d98680dc222af7b5397d9c6f904796124162
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-05-27 19:40:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,13 @@
 # Uncomment a feed source
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com//kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com//kiddin9/openwrt-packages' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
 #git clone https://github.com/kiddin9/openwrt-packages
@@ -27,24 +33,3 @@
 
 git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
 
-#克隆passwall环境插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/helloworld
-#git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-#git clone --depth=1 https://github.com/derisamedia/luci-theme-alpha package/luci-theme-alpha
-#克隆的源码放在small文件夹,预先建立small文件夹
-mkdir package/small
-pushd package/small
-
-#克隆源码
-
-#passwall2
-#git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
-#git clone -b luci-smartdns-dev --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-#mosdns
-#git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-#git clone --depth=1 https://github.com/fw876/helloworld.git
-
-popd
